895ba96ec5b7752fd7378da35a213567c3de57cb
Hanggi <hanggicrown@gmail.com>
2017-09-07 17:03:47 +0900

    generate

---
+++ generate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+echo "\033[1;33mstart generating the hexo...\033[0m"
+hexo generate
+
+echo "\033[1;33mcp the output files...\033[0m"
+cp -r ./public/* ./hanggi.github.io/
+
+echo "\033[1;33mDone!\033[0m"
