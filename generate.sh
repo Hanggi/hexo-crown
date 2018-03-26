85ad5d634fee3805f48a315a86885e99cd48103e
Hanggi <hanggicrown@gmail.com>
2018-03-26 15:34:13 +0900

    ggg

--- generate.sh
+++ generate.sh
@@ -10,3 +10,10 @@ echo "\033[1;33mcp the output files...\033[0m"
 cp -r ./public/* ./hanggi.github.io/
 
 echo "\033[1;33mDone!\033[0m"
+
+cd ./hanggi.github.io
+git add *
+git commit -m "commit io"
+git push orgin master
+
+echo "Git acp done!"
